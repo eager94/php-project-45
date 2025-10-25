67c15aa9297da2daf47f557c43ac175e27fc49c3
eager94 <>
2025-10-25 10:46:49 +0300

    added autoload section to composer.json, Cli.php w/ dependencies, added function greet to BrainGames

---
+++ src/Cli.php
@@ -0,0 +1,13 @@
+<?php
+
+NameSpace BrainGames\Cli;
+
+use function cli\line;
+use function cli\prompt;
+
+function greet()
+{
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, $name!");
+}
